d92f06138003788fdbcf6407d6977404bdc6d1f6
Jacob Burrell <Jacobburrell@pm.me>
2021-02-01 10:52:20 -0800

    WIP working implementation of default fee setting per currency.
    Need to float text to right and make sure send screen always pulls in default fee. Right now, user must open change mining fee scene.

--- src/components/scenes/ChangeMiningFeeScene.js
+++ src/components/scenes/ChangeMiningFeeScene.js
@@ -2,7 +2,7 @@
 
 import { type EdgeCurrencyWallet } from 'edge-core-js/types'
 import * as React from 'react'
-import { ScrollView, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native'
+import { ScrollView, StyleSheet, Switch, Text, TouchableWithoutFeedback, View } from 'react-native'
 import { Actions } from 'react-native-router-flux'
 import EntypoIcon from 'react-native-vector-icons/Entypo'
 import { connect } from 'react-redux'
@@ -12,12 +12,12 @@ import { FEE_STRINGS } from '../../constants/WalletAndCurrencyConstants.js'
 import s from '../../locales/strings.js'
 import { PrimaryButton } from '../../modules/UI/components/Buttons/PrimaryButton.ui.js'
 import { getGuiMakeSpendInfo } from '../../modules/UI/scenes/SendConfirmation/selectors.js'
-import { type FeeOption } from '../../reducers/scenes/SendConfirmationReducer.js'
+import { getSelectedCurrencyCode } from '../../modules/UI/selectors.js'
+import type { CurrencySetting, FeeOption } from '../../reducers/scenes/SettingsReducer'
 import { dayText, nightText } from '../../styles/common/textStyles.js'
 import { THEME } from '../../theme/variables/airbitz.js'
-import { type Dispatch, type RootState } from '../../types/reduxTypes.js'
+import type { Dispatch, RootState } from '../../types/reduxTypes.js'
 import { FormField } from '../common/FormField.js'
-import CheckBox from '../../modules/UI/components/CheckBox/CheckBox.ui.js'
 import { SceneWrapper } from '../common/SceneWrapper.js'
 
 type OwnProps = {
@@ -26,36 +26,45 @@ type OwnProps = {
 
 type StateProps = {
   networkFeeOption?: FeeOption,
-  defaultFee: Boolean,
-  customNetworkFee?: Object
+  customNetworkFee?: Object,
+  currencySettings: CurrencySetting
 }
 
 type DispatchProps = {
-  onSubmit(networkFeeOption: string, customNetworkFee: Object): mixed
+  onSubmit(networkFeeOption: FeeOption, customNetworkFee: Object, setDefault: boolean, currencyCode: string): mixed
 }
 
 type Props = OwnProps & StateProps & DispatchProps
 
 type State = {
   networkFeeOption: FeeOption,
-  defaultFee: Boolean,
-  customNetworkFee: Object
+  customNetworkFee: Object,
+  currencyCode: string,
+  setDefault: boolean
 }
 
 export class ChangeMiningFee extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props)
-    const { networkFeeOption = 'standard', customNetworkFee = {} } = props
+    let { networkFeeOption = 'standard', customNetworkFee = {} } = props // Initially standard
+
+    const setDefault = !(props.currencySettings?.defaultFee == null) // Set false if null, true if exists
+
+    if (!(props.currencySettings?.defaultFee == null)) {
+      networkFeeOption = props.currencySettings.defaultFee // Set default fee
+    }
+
+    const currencyCode = props.wallet.currencyInfo.currencyCode
     const customFormat = this.getCustomFormat()
 
     if (customFormat != null && Object.keys(customNetworkFee).length !== customFormat.length) {
       // Reset the custom fees if they don't match the format:
       const defaultCustomFee = {}
       for (const key of customFormat) defaultCustomFee[key] = ''
-      this.state = { networkFeeOption, customNetworkFee: defaultCustomFee }
+      this.state = { networkFeeOption, customNetworkFee: defaultCustomFee, currencyCode, setDefault }
     } else {
       // Otherwise, use the custom fees from before:
-      this.state = { networkFeeOption, customNetworkFee }
+      this.state = { networkFeeOption, customNetworkFee, currencyCode, setDefault }
     }
   }
 
@@ -68,19 +77,20 @@ export class ChangeMiningFee extends React.Component<Props, State> {
   }
 
   onSubmit = () => {
-    const { networkFeeOption, customNetworkFee } = this.state
-    this.props.onSubmit(networkFeeOption, customNetworkFee)
+    const { networkFeeOption, customNetworkFee, setDefault, currencyCode } = this.state
+    this.props.onSubmit(networkFeeOption, customNetworkFee, setDefault, currencyCode)
     Actions.pop()
   }
 
   render() {
     const customFormat = this.getCustomFormat()
 
-    let defaultFee = false
-
     return (
       // Add checkbox
       <SceneWrapper background="body" hasTabs={false} avoidKeyboard>
+        {/* <View>
+          <Text>Set Default Fee for {this.state.currencyCode}</Text>
+        </View> */}
         <ScrollView style={styles.content}>
           {this.renderRadioRow('high', s.strings.mining_fee_high_label_choice)}
           {this.renderRadioRow('standard', s.strings.mining_fee_standard_label_choice)}
@@ -89,22 +99,22 @@ export class ChangeMiningFee extends React.Component<Props, State> {
           {customFormat != null ? this.renderCustomFee(customFormat) : null}
           {this.renderFeeWarning()}
 
-          
           <View>
             <TouchableWithoutFeedback>
-              <View style={styles.touchableCheckboxInterior}>
-                <CheckBox onPress={() => this.toggleDefaultFee()} style={styles.checkBox} enabled={this.state.defaultFee} />
+              <View style={styles.paddingLeftIcon}>
+                <Switch value={this.state.setDefault} onChange={() => this.toggleDefaultFee()} />
               </View>
             </TouchableWithoutFeedback>
-            <View>
-              <Text style={styles.tokenNameText}>{'Set Default Fee'}</Text>
+            <View style={styles.paddingRightIcon}>
+              <Text>
+                Default Fee for {this.state.currencyCode} is {this.state.setDefault ? 'Enabled' : 'Disabled'}
+              </Text>
             </View>
           </View>
 
           <PrimaryButton onPress={this.onSubmit} style={styles.saveButton}>
             <PrimaryButton.Text>{s.strings.save}</PrimaryButton.Text>
           </PrimaryButton>
-          
         </ScrollView>
       </SceneWrapper>
     )
@@ -123,16 +133,10 @@ export class ChangeMiningFee extends React.Component<Props, State> {
     )
   }
 
-  toggleDefaultFee() {
-    const { defaultFee } = this.state
-
-    return (
-      <TouchableWithoutFeedback>
-        <View style={styles.touchableCheckboxInterior}>
-          <CheckBox style={styles.checkBox} enabled={!defaultFee} />
-        </View>
-      </TouchableWithoutFeedback>
-    )
+  toggleDefaultFee = () => {
+    this.setState(prevState => ({
+      setDefault: !prevState.setDefault
+    }))
   }
 
   renderCustomFee(customFormat: string[]): React.Node {
@@ -213,21 +217,39 @@ const rawStyles = {
     flexDirection: 'column',
     justifyContent: 'space-between'
   },
+  paddingRightIcon: {
+    paddingLeft: THEME.rem(0.75)
+  },
+  paddingLeftIcon: {
+    paddingRight: THEME.rem(0.75)
+  },
 
   saveButton: {
     marginTop: THEME.rem(1.25)
   }
 }
 const styles: typeof rawStyles = StyleSheet.create(rawStyles)
-
 export const ChangeMiningFeeScene = connect(
   (state: RootState): StateProps => ({
+    // Connector
+    // Selector
     networkFeeOption: getGuiMakeSpendInfo(state).networkFeeOption,
-    customNetworkFee: getGuiMakeSpendInfo(state).customNetworkFee
+    customNetworkFee: getGuiMakeSpendInfo(state).customNetworkFee,
+    currencySettings: state.ui.settings[getSelectedCurrencyCode(state)]
   }),
   (dispatch: Dispatch): DispatchProps => ({
-    onSubmit(networkFeeOption: string, customNetworkFee: Object) {
+    // dispatch = setter
+    onSubmit(networkFeeOption: FeeOption, customNetworkFee: Object, setDefault: boolean, currencyCode: string) {
+      if (setDefault) {
+        console.log(`Has default fee: ${setDefault.toString()}`)
+        dispatch({
+          type: 'UI/SETTINGS/SET_DEFAULT_FEE',
+          data: { currencyCode: currencyCode, defaultFee: networkFeeOption, customFee: customNetworkFee }
+        })
+      }
       dispatch(sendConfirmationUpdateTx({ networkFeeOption, customNetworkFee }))
     }
   })
 )(ChangeMiningFee)
+
+// add pulling in default fee at send confirmation screen.
